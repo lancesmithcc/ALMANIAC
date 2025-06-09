607a3b51c17e51fae609f7d7d68e4806566fe557
genexlance <lance@genexmarketing.com>
2025-06-08 19:46:24 -0700

    again

--- src/app/api/gardens/route.ts
+++ src/app/api/gardens/route.ts
@@ -27,6 +27,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(_request: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
